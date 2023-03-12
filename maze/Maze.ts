8abd4987b9b490356db37abbac866b8d6da24a57
David Viggiano <91092410+dviggiano@users.noreply.github.com>
2023-03-11 20:29:02 -0600

    Update Maze.ts

--- maze/Maze.ts
+++ maze/Maze.ts
@@ -31,7 +31,7 @@ export default class Maze {
         if (random) {
             this.randomize();
         }
-    };
+    }
 
     async build(id: string) {
         const mazesRef = collection(db, 'mazes');
@@ -40,7 +40,7 @@ export default class Maze {
         const docData = docSnap.data()!;
 
         this.load(docData.template);
-    };
+    }
 
     adjacentTo(space: Space): Space[] {
         const adjacent = [];
@@ -70,7 +70,7 @@ export default class Maze {
         }
 
         return adjacent;
-    };
+    }
 
     load(template: Object) {
         for (let x = 0; x < this.spaces.length; x++) {
@@ -94,7 +94,7 @@ export default class Maze {
         }
 
         return this;
-    };
+    }
 
     getTemplate() {
         const template = {};
@@ -136,82 +136,7 @@ export default class Maze {
         }
 
         return template;
-    };
-
-    testRandomize() {
-        // shuffle spaces using the Fisher-Yates algorithm on all spaces other than the entrance and exit
-        const spaces = [];
-
-        for (const column of this.spaces) {
-            for (const space of column) {
-                if (this.entrance !== space && this.exit !== space) {
-                    spaces.push(space);
-                }
-            }
-        }
-
-        let i = spaces.length;
-
-        while (i > 0) {
-            const j = Math.floor(Math.random() * i--);
-            spaces[i], spaces[j] = spaces[j], spaces[i];
-        }
-
-        spaces.push(this.entrance); // add the entrance to the end of the array so the helper method grabs it first
-
-        const total = [];
-
-        // call recursive helper method to build maze, starting by attempting to connect the entrance to the exit
-        this.testLeavesUp(spaces, new Set([...spaces]), new Set([this.exit]), total);
-
-        return total.length;
-    };
-
-    testLeavesUp(unvisitedStack: Space[], unvisited: Set<Space>, path: Set<Space>, total) {
-        let space;
-
-        // filter the stack of unvisited spaces
-        do {
-           space = unvisitedStack.pop()!; // grabs the entrance on initial call
-        } while (!unvisited.has(space));
-
-        const orderedBranch: Space[] = [];
-        const branch = new Set<Space>();
-
-        // pick random adjacent spaces to connect to until the final maze is reached
-        while (!path.has(space)) {
-            unvisited.delete(space);
-            orderedBranch.push(space);
-            branch.add(space);
-            space = this.testLeavesUpHelper(orderedBranch, branch, path, 1, total);
-        }
-
-        for (const space of branch) {
-            path.add(space);
-        }
-
-        if (unvisited.size !== 0) {
-            this.leavesUp(unvisitedStack, unvisited, path);
-        }
-    };
-
-    testLeavesUpHelper(orderedBranch: Space[], branch: Set<Space>, path: Set<Space>, calls: number, total): Space {
-        total.push(0);
-        const space = orderedBranch[orderedBranch.length - calls]; // backtrack if calls > 1
-        // adjacent spaces are valid if they are not already in the current branch
-        // the exit is only valid before any branch has been added to the final maze as it should only connect to 1 space
-        let adjacent: Space[] = this.adjacentTo(space).filter(neighbor =>
-            (neighbor !== this.exit || path.size === 1) && !branch.has(neighbor)
-        );
-        // if no valid adjacent spaces exist, backtrack through the branch and move in another direction
-        if (adjacent.length === 0) {
-            return this.leavesUpHelper(orderedBranch, branch, path, calls + 1);
-        }
-        // grab and connect a random space from valid adjacent spaces
-        const neighbor = adjacent[Math.floor(Math.random() * adjacent.length)];
-        space.addEdge(neighbor);
-        return neighbor;
-    };
+    }
 
     randomize() {
         // shuffle spaces using the Fisher-Yates algorithm on all spaces other than the entrance and exit
@@ -236,7 +161,7 @@ export default class Maze {
 
         // call recursive helper method to build maze, starting by attempting to connect the entrance to the exit
         this.leavesUp(spaces, new Set([...spaces]), new Set([this.exit]));
-    };
+    }
 
     leavesUp(unvisitedStack: Space[], unvisited: Set<Space>, path: Set<Space>) {
         let space;
@@ -266,7 +191,7 @@ export default class Maze {
         if (unvisited.size !== 0) {
             this.leavesUp(unvisitedStack, unvisited, path);
         }
-    };
+    }
 
     leavesUpHelper(orderedBranch: Space[], branch: Set<Space>, path: Set<Space>, calls: number): Space {
         const space = orderedBranch[orderedBranch.length - calls]; // backtrack if calls > 1
@@ -283,7 +208,7 @@ export default class Maze {
         const neighbor = adjacent[Math.floor(Math.random() * adjacent.length)];
         space.addEdge(neighbor);
         return neighbor;
-    };
+    }
 
     containsNoCycles(): boolean {
         const visited = new Set<Space>();
@@ -297,7 +222,7 @@ export default class Maze {
         }
 
         return true;
-    };
+    }
 
     isFullyTraversable(): boolean {
         const toVisit = [this.entrance];
@@ -320,22 +245,5 @@ export default class Maze {
 
     isValid(): boolean {
         return this.containsNoCycles() && this.isFullyTraversable() && this.exit.connected.length === 1;
-    };
-};
-
-// for (let i = 2; i < 50; i++) {
-//     const operations = [];
-//  
-//     for (let j = 0; j < 1024; j++) {
-//         const maze = new Maze(i);
-//         operations.push(maze.testRandomize());
-//     }
-//
-//     let total = 0;
-//
-//     for (const operation of operations) {
-//         total += operation;
-//     }
-//
-//     console.log(`${i * 2},${total / operations.length}`)
-// }
+    }
+}
